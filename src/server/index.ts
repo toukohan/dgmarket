ad64ca20a7f83b93a163b7fa84b67d403af1f28e
Touko Hänninen <touko.j.hanninen@student.jyu.fi>
2025-12-28 12:52:11 +0200

    initial server

---
+++ src/server/index.ts
@@ -0,0 +1,27 @@
+import express from 'express'
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.json({message: "Hello world"})
+})
+
+const start = async () => {
+
+    try {
+
+        app.listen(port, () => {
+            console.log("Server started at port", port)
+        })
+    } catch (err) {
+        console.error("Server failed to start:", err)
+    }
+
+
+}
+
+start()
